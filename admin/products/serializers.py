7393339244ae452372ad846226615de3f5e02608
1Mashilo <christophermashilo@gmail.com>
2024-01-25 13:53:16 +0200

    Added documentation to ProductSerializer

--- admin/products/serializers.py
+++ admin/products/serializers.py
@@ -3,5 +3,12 @@ from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
+        """
+        Meta class for ProductSerializer.
+
+        Attributes:
+            model (class): The model class to serialize.
+            fields (list): The fields to include in the serialized output.
+        """
         model = Product
         fields = '__all__'
